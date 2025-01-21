04435e27af7c6dc27ecf64ced0501562a18699b8
GroupGrid <groupgridme@gmail.com>
2025-01-21 10:36:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript to handle the "Get Started" button
+document.getElementById('signupBtn').addEventListener('click', function() {
+    window.location.href = "sign_up.html"; // Redirect to signup page
+});
+document.getElementById('logBTn').addEventListener('click', function() {
+    window.location.href = "login.html"; // Redirect to signup page
+});
